ac4768ff087e02166e1bc5853f708518f49cd418
gldnstrn <skrantz822@gmail.com>
2018-11-10 20:24:09 -0500

    Make an audio testing scene
    Testing out multiple audio sources on cylinder mesh and triggering them based on user input - s

---
+++ XRBook/Assets/Audio/AudioScripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioClip audioClip;
+    public AudioClip liftOffAudioClip;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+	void Start () {
+        audioSource.clip = audioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    // Should change this to on componentOverlap with hand
+        if (Input.GetKeyDown(KeyCode.Space))
+            audioSource.Stop();
+        
+	}
+}
